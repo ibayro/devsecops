d12b50053b95e33ec5b76ffecc56d11c82c77782
Ihor <104074570+ibayro@users.noreply.github.com>
2023-06-24 14:46:44 +0300

    Create pre-commit.sh
    Implemented pre-commit hook script with automatic installation of gitleaks depending on the OS, with the ENABLE option using git config and installation using the “curl pipe sh” method

---
+++ pre-commit.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+command -v gitleaks >/dev/null 2>&1 || { 
+    echo >&2 "Gitleaks is not found. Installing Gitleaks on a local machine.";
+    
+    # Installing Gitleaks takes into consideration the OS type of a local machine.
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        curl -sSfL https://github.com/gitleaks/gitleaks/releases/download/v8.17.0/gitleaks_8.17.0_linux_x64.tar.gz | tar -xvzf
+        export PATH=$(pwd):$PATH
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        curl -sSfL https://github.com/gitleaks/gitleaks/releases/download/v8.17.0/gitleaks_8.17.0_darwin_arm64.tar.gz | tar -xvzf
+        export PATH=$(pwd):$PATH
+    elif [[ "$OSTYPE" == "win32"* ]]; then
+        curl -sSfL https://github.com/gitleaks/gitleaks/releases/download/v8.17.0/gitleaks_8.17.0_windows_arm64.zip | tar -xvzf
+        export PATH=$(pwd):$PATH
+     elif [[ "$OSTYPE" == "cygwin"* ]]; then
+        curl -sSfL https://github.com/gitleaks/gitleaks/releases/download/v8.17.0/gitleaks_8.17.0_windows_arm64.zip | tar -xvzf
+        export PATH=$(pwd):$PATH
+    else
+        echo >&2 "Couldn't install gitleaks. Unknown OS is detected."; 
+        exit 1;
+    fi
+}
+
+ENABLE=$(git config --bool hooks.gitleaks-enable)
+
+# ENABLE option check
+if [ "$ENABLE" != "true" ]; then
+    echo "Advance: Re-verification of the visibility of secrets in the code of the code. To enable, type the command: git config hooks.gitleaks-enable true"
+    exit 0
+fi
+
+if [ $? -eq 0 ]; then
+    echo "Secret has been detected. Sensitive data will not pass. Take proper security actions in order to proceed."
+    exit 1
+fi
+
+exit 0
